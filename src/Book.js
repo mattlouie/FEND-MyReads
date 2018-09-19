3ca24189c98560773f2b384cf30110756df1a00a
Matthew Louie <mlouie915@gmail.com>
2018-09-18 20:57:03 -0700

    Refactored render in Book.js

--- src/Book.js
+++ src/Book.js
@@ -17,7 +17,6 @@ class Book extends Component {
   render() {
     const {thisBook, bookShelf} = this.props
 
-    if(thisBook.imageLinks) {
         return(
             <li>
                 <div className="book">
@@ -32,7 +31,6 @@ class Book extends Component {
                                 backgroundPosition: 'center'
                             }}
                         >
-
                         </div>
                        <div className="book-shelf-changer">
                             <select onChange={(event) => {this.changeShelf(thisBook, event.target.value)}}
@@ -50,41 +48,7 @@ class Book extends Component {
                 </div>
             </li>
         )
-    } else {
-        return(
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div
-                            className="book-cover"
-                            style={{
-                                width: 128,
-                                height: 193,
-                                backgroundImage: `url(https://ibf.org/site_assets/img/placeholder-book-cover-default.png)`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center'
-                            }}
-                        >
-
-                        </div>
-                        <div className="book-shelf-changer">
-                            <select onChange={(event) => {this.changeShelf(thisBook, event.target.value)}}
-                                    value={thisBook.shelf}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="remove">Remove</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{thisBook.title}</div>
-                    <div className="book-authors">{thisBook.authors ? thisBook.authors.toString() : 'Nothing!'}</div>
-                </div>
-            </li>
-        )
     }
   }
-}
 
 export default Book
